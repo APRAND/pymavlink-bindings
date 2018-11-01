c89f1241655ea518167fc0290056e19529dcbe63
Samuel Dudley <Dudley.Samuel@gmail.com>
2018-11-01 14:36:49 +1100

    Update and rename build.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-
-#sudo pip3 install -r requirements.txt
+# sudo pip3 install -r requirements.txt
 git submodule update --init --recursive
 echo "Generating mavlink headders..."
 python3 ./mavlink/pymavlink/tools/mavgen.py --lang C++11 ./mavlink/message_definitions/v1.0/ardupilotmega.xml -o ./generated/mavlink --wire-protocol=2.0
-
-#python3 ./mavlink/pymavlink/tools/mavgen.py --lang Python ./mavlink/message_definitions/v1.0/ardupilotmega.xml -o ./generated/mavlink --wire-protocol=2.0
+# python3 ./mavlink/pymavlink/tools/mavgen.py --lang Python ./mavlink/message_definitions/v1.0/ardupilotmega.xml -o ./generated/mavlink --wire-protocol=2.0
 
 
